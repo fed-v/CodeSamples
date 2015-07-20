1375ec4082144e6f7064306912daf399047bec51
fED <fed_v@hotmail.com>
2015-07-20 12:16:06 -0400

    Improved Bootstrap Transparent Navbar

--- Bootstrap/Transparent-navbar-and-background-image/js/script.js
+++ Bootstrap/Transparent-navbar-and-background-image/js/script.js
@@ -1,11 +1,42 @@
 jQuery(document).ready(function( $ ) {
-	
+
+	//CREATE INSTANCE OF THE DATE OBJECT
+	var date = new Date();
+
+	//EXTRACT FOUR DIGIT YEAR AND ADD TO SPAN
+	document.getElementById('year').innerHTML = date.getFullYear();
+
 	$(window).scroll(function() {
 		if ($(document).scrollTop() > 50) {
 			$('.navbar').addClass('shrink');
 		}else {
-			$('.navbar').removeClass('shrink'); 
+			$('.navbar').removeClass('shrink');
 		}
 	});
-			
+
+
+	// HAMBURGER ROLLOVER
+	$(".navbar-default .navbar-toggle").on('mouseover', function() {
+		//$(".navbar-default .navbar-toggle .icon-bar").css('background', '#840000').animate("slow");
+		$(".navbar-default .navbar-toggle .icon-bar").animate({ "backgroundColor": "#840000" }, "slow" );
+	});
+
+	$(".navbar-default .navbar-toggle").on('mouseleave', function() {
+		$(".navbar-default .navbar-toggle .icon-bar").animate({ "backgroundColor": "#888" }, "slow" );
+	});
+
+	// HAMBURGER CLICK
+	$(".navbar-default .navbar-nav > li > a").on('click', function(event) {
+		//event.preventDefault();
+		//alert("Close!");
+
+		/*$(".navbar-collapse").fadeOut('slow');*/
+
+	});
+
+	/*
+	$(".navbar-default .navbar-toggle").on('click', function() {
+		$(".navbar-collapse").fadeIn('slow');
+	});*/
+
 });
